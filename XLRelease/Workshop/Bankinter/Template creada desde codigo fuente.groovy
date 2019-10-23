633de08be63d5ba64f3c2255369449ac1494d1fb
jclopeza <42203369+jclopeza@users.noreply.github.com>
2019-10-23 07:56:03 +0200

    Ejemplo template codigo fuente

---
+++ XLRelease/Workshop/Bankinter/Template creada desde codigo fuente.groovy
@@ -0,0 +1,24 @@
+// Exported from:        http://kubuntu:5516/#/templates/Folder69f85172321f46a89580c80c09103603-Folder0c3ec44ba3194f6d932dc97005e007c5-Release3e2a20936a554f31a28c11f27f313a39/releasefile
+// XL Release version:   9.0.6
+// Date created:         Wed Oct 23 07:56:01 CEST 2019
+
+xlr {
+  template('Template creada desde codigo fuente') {
+    folder('Workshop/Bankinter')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-10-23T09:00:00+0200')
+    phases {
+      phase('Template') {
+        color '#0099CC'
+        tasks {
+          manual('Step 1') {
+            
+          }
+          manual('Step 2') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
